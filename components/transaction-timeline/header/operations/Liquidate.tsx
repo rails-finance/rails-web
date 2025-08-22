b5770897ca119b537862853fc9c4c47f76624244
Miles Essex <137334283+milodonid@users.noreply.github.com>
2025-08-22 13:48:36 +0100

    added values into liquidate transaction header

--- components/transaction-timeline/header/operations/Liquidate.tsx
+++ components/transaction-timeline/header/operations/Liquidate.tsx
@@ -3,12 +3,13 @@ import { OperationBadge } from "../components/OperationBadge";
 import { AssetAction } from "../components/AssetAction";
 
 export function LiquidateHeader({ tx }: { tx: TroveLiquidationTransaction }) {
+  const { collChangeFromOperation, debtChangeFromOperation } = tx.troveOperation;
   return (
     <>
       <OperationBadge label="LIQUIDATION" color="red" />
       <div className="flex items-center gap-1">
-        <AssetAction action="Liquidated" asset={tx.collateralType} />
-        <AssetAction action="Cleared" asset={tx.assetType} />
+        <AssetAction action="Liquidated" asset={tx.collateralType} amount={Math.abs(collChangeFromOperation)} alwaysShowAmount />
+        <AssetAction action="Cleared" asset={tx.assetType} amount={Math.abs(debtChangeFromOperation)} alwaysShowAmount />
       </div>
     </>
   );
