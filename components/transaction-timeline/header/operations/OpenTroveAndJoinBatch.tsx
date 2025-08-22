831e223ddc1bd45f4743116578034e916d0ef102
Miles Essex <137334283+milodonid@users.noreply.github.com>
2025-08-22 12:13:28 +0100

    added values into openTroveAndJoinBatch transaction header

--- components/transaction-timeline/header/operations/OpenTroveAndJoinBatch.tsx
+++ components/transaction-timeline/header/operations/OpenTroveAndJoinBatch.tsx
@@ -3,8 +3,10 @@ import { OperationBadge } from "../components/OperationBadge";
 import { InterestRateBadge } from "../components/InterestRateBadge";
 import { BatchManagerInfo } from "../components/BatchManagerInfo";
 import { BatchIcon } from "../components/BatchIcon";
+import { AssetAction } from "../components/AssetAction";
 
 export function OpenTroveAndJoinBatchHeader({ tx }: { tx: TroveTransaction }) {
+  const { collChangeFromOperation, debtChangeFromOperation } = tx.troveOperation;
   return (
     <div>
       <div className="flex items-center flex-wrap gap-2">
@@ -15,6 +17,8 @@ export function OpenTroveAndJoinBatchHeader({ tx }: { tx: TroveTransaction }) {
             icon={<BatchIcon className="w-3 h-3 mr-1" />}
           />
           <InterestRateBadge rate={tx.stateAfter.annualInterestRate} />
+          <AssetAction action="Supply" asset={tx.collateralType} amount={collChangeFromOperation} />
+          <AssetAction action="Borrow" asset={tx.assetType} amount={debtChangeFromOperation} />
         </div>
       </div>
       <BatchManagerInfo batchManager={tx.stateAfter.interestBatchManager} color="blue" />
