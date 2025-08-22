9d21c20bd410af0074bfccfa08ea9f3ceed18026
Miles Essex <137334283+milodonid@users.noreply.github.com>
2025-08-22 12:16:10 +0100

    added values into closeTrove transaction header

--- components/transaction-timeline/header/operations/CloseTrove.tsx
+++ components/transaction-timeline/header/operations/CloseTrove.tsx
@@ -3,12 +3,13 @@ import { OperationBadge } from "../components/OperationBadge";
 import { AssetAction } from "../components/AssetAction";
 
 export function CloseTroveHeader({ tx }: { tx: TroveTransaction }) {
+  const { collChangeFromOperation, debtChangeFromOperation } = tx.troveOperation;
   return (
     <>
       <OperationBadge label="CLOSE" color="red" />
       <div className="flex items-center gap-1">
-        <AssetAction action="Repay" asset={tx.assetType} />
-        <AssetAction action="Withdraw" asset={tx.collateralType} />
+        <AssetAction action="Repay" asset={tx.assetType} amount={Math.abs(debtChangeFromOperation)} />
+        <AssetAction action="Withdraw" asset={tx.collateralType} amount={Math.abs(collChangeFromOperation)} />
       </div>
     </>
   );
