d38bbeda34403936832cc20cba3930ba7ad72d19
Miles Essex <137334283+milodonid@users.noreply.github.com>
2025-08-22 15:14:03 +0100

    transaction details footer - remove link to 'transaction report' and move tx counter value to align right

--- components/transaction-timeline/item/components/TransactionFooter.tsx
+++ components/transaction-timeline/item/components/TransactionFooter.tsx
@@ -11,18 +11,10 @@ interface TransactionFooterProps {
 export function TransactionFooter({ timestamp, txIndex, txHash, isExpanded }: TransactionFooterProps) {
   return (
     <div className={"flex justify-between items-center"}>
-      <div className="text-xs text-slate-600 font-bold flex items-center">
-        <span className="mr-1 px-1 text-slate-500 rounded bg-slate-950/30 inline-block">{txIndex}</span>
+      <div className="text-xs text-slate-600 font-bold">
         {formatTimestamp(timestamp)}
       </div>
-      {isExpanded && txHash && (
-        <Button href="#" variant="primary" className="p-2 w-full sm:w-auto">
-          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-            <path d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
-            <path d="M13 3v5a2 2 0 002 2h5"></path>
-          </svg>
-        </Button>
-      )}
+      <span className="px-1 text-xs text-slate-500 rounded bg-slate-950/30">{txIndex}</span>
     </div>
   );
 }
