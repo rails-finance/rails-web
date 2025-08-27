c03a397589fb8b9d3bfdcadd910bcbd3f1b18b70
Miles Essex <137334283+milodonid@users.noreply.github.com>
2025-08-27 14:45:51 +0100

    debtMetric add upfront fee tp state grid

--- components/transaction-timeline/state-grid/metrics/DebtMetric.tsx
+++ components/transaction-timeline/state-grid/metrics/DebtMetric.tsx
@@ -8,21 +8,33 @@ interface DebtMetricProps {
   before?: number;
   after: number;
   isCloseTrove: boolean;
+  upfrontFee?: number;
 }
 
-export function DebtMetric({ assetType, before, after, isCloseTrove }: DebtMetricProps) {
+export function DebtMetric({ assetType, before, after, isCloseTrove, upfrontFee }: DebtMetricProps) {
   const hasChange = before && before !== after;
   return (
     <StateMetric label="Debt" icon={<TokenIcon assetSymbol={assetType} className="mr-2 w-5 h-5 text-green-400" />}>
-      <StateTransition>
-        {hasChange && (
-          <>
-            <div className="text-slate-600">{before}</div>
-            <TransitionArrow />
-          </>
+      <div>
+        <StateTransition>
+          {hasChange && (
+            <>
+              <div className="text-slate-600">{before}</div>
+              <TransitionArrow />
+            </>
+          )}
+          <div className="flex">
+            <div className="flex flex-row">
+              {isCloseTrove ? <ClosedStateLabel /> : <div className="text-sm font-semibold text-white">{after}</div>}
+            </div>
+          </div>
+        </StateTransition>
+        {upfrontFee !== undefined && upfrontFee > 0 && (
+          <div className="">
+            <span className="text-xs text-slate-500 mt-0.5 block">Fee: {upfrontFee}</span>
+          </div>
         )}
-        {isCloseTrove ? <ClosedStateLabel /> : <div className="text-sm font-semibold text-white">{after}</div>}
-      </StateTransition>
+      </div>
     </StateMetric>
   );
 }
